afd3f82e435320a66f7245c27f0b7f1ce8bb295f
16778 <1677829079@qq.com>
2025-07-11 11:51:00 +0800

    initialize configuration(dependencies,database)

---
+++ src/main/java/com/Vilingo/VilingoApplication.java
@@ -0,0 +1,13 @@
+package com.Vilingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vilingo/VilingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vilingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
